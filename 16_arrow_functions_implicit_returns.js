d172f757253273e52b9aefab3fbf0f4c11190906
Ashutosh Kumar <75922918+ashusnapx@users.noreply.github.com>
2022-06-11 15:22:55 +0530

    Callbacks & Array Methods

---
+++ 16_arrow_functions_implicit_returns.js
@@ -0,0 +1,14 @@
+const rollDie = () =>
+(
+    Math.floor(Math.random() * 6) + 1
+)
+
+
+const add = (a, b) =>
+(
+    a + b
+)
+
+const add = (a, b) => a + b
+const sqaure = num => num * num
+const cube = num => num ** 3
