90dc9eab55f42a499f7c049f09a1031f8614b26c
Ashutosh Kumar <75922918+ashusnapx@users.noreply.github.com>
2022-06-11 12:49:15 +0530

    Callbacks & Array Methods

---
+++ 14_arrow_functions.js
@@ -0,0 +1,29 @@
+const add = function (x, y)
+{
+    return x + y;
+}
+
+function add(x, y)
+{
+    return x + y;
+}
+
+const add = (x, y) =>
+{
+    return x + y;
+}
+
+const sqaure = x =>
+{
+    return x * x;
+}
+
+const cube = x =>
+{
+    return x ** 3;
+}
+
+const rollDie = () =>
+{
+    return Math.floor(Math.random() * 6) + 1;
+}
